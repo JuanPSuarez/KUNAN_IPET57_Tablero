21fd75a82c25596c41bcefab311944b83a64d793
josefrias553 <129226812+josefrias553@users.noreply.github.com>
2023-09-12 09:18:03 -0300

    Add files via upload

---
+++ redmine_api.py
@@ -0,0 +1,11 @@
+from redminelib import Redmine
+import urllib3
+
+
+def connect_to_redmine(redmine_url, redmine_key):
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    return Redmine(redmine_url, key=redmine_key, requests={'verify': False})
+
+def get_issues(redmine, project_id):
+    return redmine.issue.filter(project_id=project_id, status_id=5)
