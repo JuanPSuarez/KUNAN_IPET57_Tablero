a81ffed64bcfd34696936f4acbc794845975bf91
tomas <tomycar14cba@gmail.com>
2023-10-02 11:15:48 -0300

    tarea de Santiago Integrada

--- proyectoflask/app/app.py
+++ proyectoflask/app/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 
 app=Flask(__name__)
-app.config['TEMPLATE_FOLDER']='templates'
+app.config['TEMPLATES_FOLDER']='templates'
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def landing():
+    return render_template('landing.html')
+
+@app.route('/reporter')
+def reporter():
+    return render_template('reporter.html')
 
 if __name__ == '__main__':
     app.run()
